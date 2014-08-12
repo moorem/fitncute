6d16b99ae4caeaf338a4aa32c3bf6f0b3b07ef5e
thiyagarajan <thiyagarajannv@yahoo.com>
2014-08-12 19:22:35 +0530

    added default country as India

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -12,9 +12,11 @@ Spree.config do |config|
   config.allow_guest_checkout = false
   config.currency = 'INR'
   config.mails_from = 'admin@fitncute.com'
+  country = Spree::Country.find_by_name('India') if Spree::Country.table_exists?
+  config.default_country_id = country.id if country.present?
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
 end
 
-Spree.user_class = "Spree::User"
+Spree.user_class = 'Spree::User'
