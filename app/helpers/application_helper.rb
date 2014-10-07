6bbbd3ade7dd124f9206f84f645313bb7a78b2ee
thiyagarajan <thiyagarajannv@yahoo.com>
2014-10-07 13:28:45 +0530

    minor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,15 @@ module ApplicationHelper
       'inactive'
     end
   end
+
+  def taxons_tree(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.children.empty?
+    content_tag :div, class: 'list-group dropdown' do
+      root_taxon.children.map do |taxon|
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item dropdown-toggle'
+        render 'spree/shared/subcatecory'
+        link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
+      end.join("\n" + (render 'spree/shared/subcatecory')).html_safe
+    end
+  end
 end
