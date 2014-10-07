31c83bff94c8acf36ce4ff91c7c54c9a496e2577
thiyagarajan <thiyagarajannv@yahoo.com>
2014-10-07 20:31:50 +0530

    fixes

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Taxonomy.class_eval do
+
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
       csv << column_names
@@ -12,25 +13,28 @@ Spree::Taxonomy.class_eval do
     ActiveRecord::Base.connection.execute("TRUNCATE spree_taxonomies RESTART IDENTITY")
     ActiveRecord::Base.connection.execute("TRUNCATE spree_taxons RESTART IDENTITY")
     line = 0
-    CSV.foreach(file.path, headers: true) do |row|
+    CSV.foreach(file.path, headers: true, encoding: "iso-8859-1:UTF-8") do |row|
       unless line == 0
         taxonomy_hash = row.to_hash
-        taxonomy_hash.delete_at(0)
         taxonomy_hash.compact!
-        taxonomy = Spree::Taxonomy.find_or_create_by_name(taxonomy_hash[1])
-        if taxonomy_hash.size > 2
-          parent_hash = taxonomy_hash.last 2
-          parent_taxon = Spree::Taxon.find_by_name(parent_hash[0])
-          begin
-            children_taxon = parent_taxon.children.create!(:name => parent_hash[1])
-            children_taxon.display = taxonomy_hash[0]
-            children_taxon.taxonomy = taxonomy
-            children_taxon.save
-          rescue
-          end
-        end
+        puts '**********************'
+        puts taxonomy_hash['Category Name']
+        puts '**********************'
+        taxonomy = Spree::Taxonomy.first_or_create!(name: taxonomy_hash['Category Name'])
+        # if taxonomy_hash.size > 2
+        #   parent_hash = taxonomy_hash.last 2
+        #   parent_taxon = Spree::Taxon.find_by_name(parent_hash[0])
+        #   begin
+        #     children_taxon = parent_taxon.children.create!(:name => parent_hash[1])
+        #     children_taxon.display = taxonomy_hash[0]
+        #     children_taxon.taxonomy = taxonomy
+        #     children_taxon.save
+        #   rescue
+        #   end
+        # end
       end
       line +=1
     end
   end
+
 end
